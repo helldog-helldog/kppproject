3dd2b7c247ba25c374fa4928ec5e47067b557986
stasiv <pheonix9999@gmail.com>
2017-03-10 09:22:21 +0200

    Base project.

---
+++ RahoovatorIOS/RahoovatorIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RahoovatorIOS
+//
+//  Created by MacBook Pro on 3/10/17.
+//  Copyright © 2017 Helldog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
