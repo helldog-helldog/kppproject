05eaac40e5df8d130819fbf6b6220074cfad31fe
stasiv <pheonix9999@gmail.com>
2017-04-04 12:09:30 +0300

    Added class to connect to server.

---
+++ RahoovatorIOS/Pods/Target Support Files/Pods-RahoovatorIOS/Pods-RahoovatorIOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RahoovatorIOS : NSObject
+@end
+@implementation PodsDummy_Pods_RahoovatorIOS
+@end
---
+++ RahoovatorIOS/RahoovatorIOS/Models/ServerAPIManager.swift
@@ -0,0 +1,19 @@
+//
+//  ServerAPIManager.swift
+//  RahoovatorIOS
+//
+//  Created by MacBook Pro on 4/4/17.
+//  Copyright © 2017 Helldog. All rights reserved.
+//
+
+import Foundation
+import SwiftSocket
+
+class ServerAPIManager {
+    
+    static private var client = TCPClient(address: "oleg.com", port: Int32(80))
+    
+    class func connectToServer() {
+        client.connect(timeout: 10)
+    }
+}
