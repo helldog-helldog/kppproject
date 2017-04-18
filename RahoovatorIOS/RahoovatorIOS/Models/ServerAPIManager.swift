56349f52391b340b3c8d5f8abce181768e598826
stasiv <pheonix9999@gmail.com>
2017-04-18 17:56:46 +0300

    Added method for reading response.

--- RahoovatorIOS/RahoovatorIOS/Models/ServerAPIManager.swift
+++ RahoovatorIOS/RahoovatorIOS/Models/ServerAPIManager.swift
@@ -11,17 +11,35 @@ import SwiftSocket
 
 class ServerAPIManager {
     
-    static private var client = TCPClient(address: "oleg.com", port: Int32(80))
+    var shared = ServerAPIManager()
     
-    class func connectToServer() {
-        client.connect(timeout: 10)
+    private var client = TCPClient(address: "oleg.com", port: Int32(80))
+    
+    func send(data: String) -> String {
+        switch client.connect(timeout: 10) {
+        case .success:
+            if let response = sendRequest(string: data, using: client) {
+                return response
+            }
+            return ""
+        case .failure(let error):
+            return error.localizedDescription
+        }
     }
     
-    class func send(data: String?) {
-        if data == nil {
-            return
-        }
+    private func sendRequest(string: String, using client: TCPClient) -> String? {
         
-        client.send(string: data!)
+        switch client.send(string: string) {
+        case .success:
+            return readResponse(from: client)
+        case .failure(let error):
+            return error.localizedDescription
+        }
+    }
+    
+    private func readResponse(from client: TCPClient) -> String? {
+        guard let response = client.read(1024*10) else { return nil }
+        
+        return String(bytes: response, encoding: .utf8)
     }
 }
